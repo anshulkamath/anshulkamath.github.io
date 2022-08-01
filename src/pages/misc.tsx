7fba5878de23e83789e0050800e2d2cf808306be
-h <anshulkam@gmail.com>
2022-07-31 19:49:25 -0700

    updated misc page text

--- src/pages/misc.tsx
+++ src/pages/misc.tsx
@@ -1,5 +1,12 @@
 import React from 'react'
 
-const MiscPage: React.FunctionComponent = () => <div>This is the miscellaneous page</div>
+import 'stylesheets/misc.css'
+
+const MiscPage: React.FunctionComponent = () => (
+  <div className='misc-container'>
+    <h3>Brb, gonna try to find some miscellaneous things to put here...</h3>
+    <p>Did you know that the two hardest things to find are miscellaneous things and fun facts?</p>
+  </div>
+)
 
 export default MiscPage
